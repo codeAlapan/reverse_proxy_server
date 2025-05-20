294bf021fc3f4cc4cd492542f9e01ecba5a2f2e5
codeAlapan <alapanpal2004@gmail.com>
2025-05-20 22:50:05 +0530

    phase 4 user model created

--- src/backend_Server/routes/authRoutes.js
+++ src/backend_Server/routes/authRoutes.js
@@ -1,37 +1,48 @@
 const express = require('express');
 const { generateToken } = require('../utils/jwtUtil');
 const authMiddleware = require('../middlewares/authMiddleware');
+const User = require('../models/User.js');
 
 const router = express.Router();
 
-router.post('/login', async (req, res) => {
+router.post('/signup', async (req, res) => {
   try {
-    const { username } = req.body;
-
-    if (!username) {
-      return res.status(400).json({ message: 'Username is required' });
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: 'Username or email already exists' });
     }
-
-    const token = await generateToken({ username });
-
-    res
-      .cookie('token', token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
-      })
-      .json({ message: 'Login successful' });
+    // hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+    // create new user
+    const newUser = new User({ username, email, password: hashPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
-    res.status(400).send('Error: ' + err.message);
+    res.status(500).json({ message: 'Signup failed', error: err.message });
   }
 });
 
-//* Protected route
-router.get('/protected', authMiddleware, (req, res) => {
+router.post('/login', async (req, res) => {
   try {
-    res.json({ message: `Hello, ${req.user.username}! You are authorized.` });
+    const { email, password } = req.body;
+    // check if user already exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    // check if password matches
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = generateToken({ id: user._id });
+    res.json({ message: 'Login successful', token });
   } catch (err) {
-    res.status(400).send('Error: ' + err.message);
+     res.status(500).json({ message: 'Login failed', error: err.message });
   }
 });
 
